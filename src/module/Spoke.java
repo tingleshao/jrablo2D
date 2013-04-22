3c84aca149477a550aafde716aa8acc834bdf818
tingleshao <tingleshao@gmail.com>
2013-04-22 02:39:39 -0400

    update

--- src/module/Spoke.java
+++ src/module/Spoke.java
@@ -10,19 +10,19 @@ package module;
  */
 public class Spoke {
     private double length = -1;
-    private Point startingPoint;
+    private Point startPoint;
     private Point endPoint;
     
     public Spoke(Point start, Point end) {
-        this.startingPoint = start;
+        this.startPoint = start;
         this.endPoint = end;
     }
     
     public double getLength() {
         if (this.length == -1) {
-            return 0.0;
+            this.length = Math.sqrt(Math.pow((endPoint.getX() - this.startPoint.getX()),2)+ Math.pow(endPoint.getY() - this.startPoint.getY(),2));
         }
-        return 0.0;
+        return this.length;
     }
             
 }
