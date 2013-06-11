c7541ff53755117aa7df24aaa082f9d167e949a3
tingleshao <tingleshao@gmail.com>
2013-06-11 13:28:37 -0400

    added an exception

--- src/module/Atom.java
+++ src/module/Atom.java
@@ -3,28 +3,41 @@
  * and open the template in the editor.
  */
 package module;
-
+import java.util.ArrayList;
+import RabloExceptions.*;
 /**
  *
  * @author Chong
  */
 public class Atom {
     
-    private double x;
-    private double y;
     private AtomType type;
+    private ArrayList<Spoke> spokes; 
     
-    public double getX() {
-        return this.x;
-    }
-    
-    public double getY() {
-        return this.y;
+    public Atom(){
+        this.type = AtomType.INSIDE;
+        spokes = new ArrayList<Spoke>();
     }
     
     public AtomType getType() {
         return this.type;
     }
     
+    public Spoke getUpperSpoke() {
+        return spokes.get(0);
+    }
+    
+    public Spoke getLowerSpoke() {
+        return spokes.get(1);
+    }
+    
+    public Spoke getMiddleSpoke() throws AtomException {
+        try {
+            return spokes.get(2);
+        }
+        catch (IndexOutOfBoundsException ioobe){
+            throw new AtomException("the atom only has two spokes!");
+        } 
+    }
     
 }
