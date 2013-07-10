c97868c1daaed4f50fe6aaa60a819f8e04505ee0
tingleshao <tingleshao@gmail.com>
2013-07-10 15:08:31 -0400

    interpolate srep locus, more

---
+++ src/RabloExceptions/JRabloInterpolationException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RabloExceptions;
+
+/**
+ *
+ * @author Chong
+ */
+public class JRabloInterpolationException extends Exception {
+     public JRabloInterpolationException() { super(); }
+     public JRabloInterpolationException(String message) { super(message); }
+     public JRabloInterpolationException(String message, Throwable cause) { super(message, cause); }
+     public JRabloInterpolationException(Throwable cause) { super(cause); }
+}
